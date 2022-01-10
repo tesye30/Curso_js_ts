d76452fac5720b34d2eaab68dffab5ae9bfbbb37
couto <tesye30@hotmail.com>
2022-01-09 23:55:49 -0300

    aula 6 variavel com let REGRAS

--- modulo_1/aula6/index.js
+++ modulo_1/aula6/index.js
@@ -1,3 +1,17 @@
+let nome; // Declarou a variável
+nome = 'Qualquer valor'; // inicializando a variável
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas (Ex: let let, let if, let console.)
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase (Ex: nomeCompletoDoCliente)
+// Case-sensitive (letra maiuscula e minuscula faz diferença)
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.    
+
+/*
 let nome =  'Lucas';
 
 console.log(nome, ' nasceu em 1984');
@@ -5,3 +19,5 @@ console.log('Em 2000',nome,'conheceu Ma ria.');
 console.log(nome,' casou-se com Maria em 2012.');
 console.log('Maria teve 1 filho com',nome, 'em 2015.');
 console.log('O filho de ',nome,' se chama Eduardo');
+
+ */
