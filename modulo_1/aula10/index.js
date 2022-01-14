1159a21cf1b3e5fe5306e9c80f86aa86ac17b4c0
couto <tesye30@hotmail.com>
2022-01-13 23:46:29 -0300

    aula 10 operadores aritmeticos de atribuicao e incremento 1.1

--- modulo_1/aula10/index.js
+++ modulo_1/aula10/index.js
@@ -1,6 +1,11 @@
 /**
  * Aritméticos
- * + Adição / Concatenação
- *  - / *
- * 
+ * + Adição / Concatenação (- / *)
+ *    ** Potenciacao
+ *  % Resto da divisao   
  */
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log(num1 + num2 * num3);
