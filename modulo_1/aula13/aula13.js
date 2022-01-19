6d556b48a443d4594c78c7279d87fe6252abe149
couto <tesye30@hotmail.com>
2022-01-18 23:55:49 -0300

    aula 13 strings

---
+++ modulo_1/aula13/aula13.js
@@ -0,0 +1,11 @@
+//String são indexadas - cada caractere tem um indice
+// 0123456
+// Uma lua 
+
+let umaString = 'Um texto';  
+
+//console.log(umaString.concat(' em um lindo dia.')); 
+//console.log(umaString + ' em um lindo dia.');
+//console.log(`${umaString} em um lindo dia.`);
+
+console.log(umaString.indexOf('texto'));
