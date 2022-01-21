3dad16b5090f96c4c6c5515fe7632ed3edf9ed15
couto <tesye30@hotmail.com>
2022-01-20 23:57:02 -0300

    exercicios strings 1.1

--- modulo_1/aula13/aula13.js
+++ modulo_1/aula13/aula13.js
@@ -8,4 +8,5 @@ let umaString = 'Um texto';
 //console.log(umaString + ' em um lindo dia.');
 //console.log(`${umaString} em um lindo dia.`);
 
-console.log(umaString.indexOf('texto'));
+//console.log(umaString.indexOf('texto'));
+console.log(umaString.toLowerCase);
