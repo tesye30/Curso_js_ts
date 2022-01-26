8e86d7826dd69edcbe736de8add7773f0483b217
couto <tesye30@hotmail.com>
2022-01-25 23:54:42 -0300

    exercicios resolvidos

--- modulo_1/aula14/index.js
+++ modulo_1/aula14/index.js
@@ -6,7 +6,11 @@ let num2 = 0.1; //number
 // num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
 // num1 = ((num1 * 100) + (num2 * 100)) / 100; // 1.0
 
-num1 = Number(num1.toFixed(2));
+//num1 += num2;
+//num1 += num2;
+//num1 += num2;
+
+//num1 = Number(num1.toFixed(2));
 
 console.log(num1.toFixed(2));
 console.log(Number.isInteger(num1));
@@ -16,6 +20,7 @@ console.log(Number.isInteger(num1));
 // console.log(num1.toString(2));
 // console.log(num1.toFixed(2));
 // console.log(Number.isInteger(num1));
-// console.log(Number.isNan(temp));
-// let temp = num1 + '5';
-// console.log(temp);
+// console.log(Number.isNaN(temp));
+//let temp = num1 + '5';
+//console.log(temp);
+
