307bc313f86ab31ab0730acdda47e146722489c0
couto <tesye30@hotmail.com>
2022-01-27 23:18:06 -0300

    arrays basico

---
+++ modulo_1/aula16/index.js
@@ -0,0 +1,34 @@
+//               0       1         2         3      4
+const alunos = ['Luiz', 'Maria', 'Joao']; // Luiza, Eduardo
+
+
+console.log(typeof alunos)
+
+//alunos.push('Luiza');
+//alunos.push('Eduardo');
+
+//console.log(alunos.slice(0, -1)); // Fatiar
+
+//delete alunos[1]; // apagar 
+//console.log(alunos);
+
+
+
+//const removido = alunos.pop(); // remove o ultimo elemento
+//const removidos = alunos.shift();// remove o primeiro elemento
+//console.log(removido);
+
+//alunos.unshift('Luiza'); // Adciona no começo
+//alunos.unshift('Fabio');
+
+//alunos.push('Otavio'); // Adiciona no fim
+//alunos[alunos.length] = 'Luiza'; // Adiciona no fim
+//alunos[alunos.length] = 'Fabio';
+//alunos[alunos.length] = 'Luana';
+
+
+//alunos[0] = 'Eduardo'; // Altera
+//alunos[3] = 'Luiza'; // Adicionando
+
+//console.log(alunos);
+//console.log(alunos[0]);
