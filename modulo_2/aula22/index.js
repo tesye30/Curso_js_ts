5f94d8c28c8a8a5e07efa4c20535da54f0e4498d
couto <tesye30@hotmail.com>
2022-02-04 23:42:52 -0300

    short circuit

--- modulo_2/aula22/index.js
+++ modulo_2/aula22/index.js
@@ -1,7 +1,11 @@
 /*
 Operadores Lógicos
-&& -> AND -> E
-|| -> OR -> OU
-! -> NOT ->
+&& -> AND -> E -> todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU  -> uma expressao precisa ser verdadeiras para retornar true
+! -> NOT -> NÃO -> Nega uma expressão (inverte)
 
 */
+
+//const expressaoAnd = true && true && true && true;
+//const expressaoOr = true || false ;
+console.log(expressaoOr);
