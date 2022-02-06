621478629f22f24a7ec427e054dd42365410b9e8
couto <tesye30@hotmail.com>
2022-02-05 23:56:33 -0300

    short circuit 1.1

---
+++ modulo_2/aula23/index.js
@@ -0,0 +1,25 @@
+/**
+ * && -> false && true -> false "o valor mesmo"
+ * || -> true || false -> vai retornar  "o valor verdadeiro"
+ * 
+ * FALSY
+ * *false
+ * 0
+ * '' "" ``
+ * null / undefined
+ * NaN
+ */
+
+
+
+
+
+
+
+
+//function falaOi () {
+//    return 'Oi';
+//}
+
+//const vaiExecutar = 'Joãozinho';
+//console.log(vaiExecutar && falaOi());
