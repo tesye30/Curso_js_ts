492a9db10722330a4f9340fb63c18481c1d4e07e
couto <tesye30@hotmail.com>
2022-02-08 00:11:26 -0300

    exercicios

---
+++ modulo_2/aula25/index.js
@@ -0,0 +1,15 @@
+const numero = 5;
+
+// Se (numero >= 0 && numero <= 5)  ocorrer , faça isso {codigo}
+// Se não faça isso {o codigo}
+// 
+
+if(numero >= 0 && numero <= 5) {
+    console.log('Sim, o numero esta entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O numero está entre 6 e 8.');
+} else if (numero >= 9 && numero <= 11) {
+    console.log('O numero está entre 9 e 11.');
+} else {
+    console.log('O numero não está entre 0 e 11.')
+}
