53d53de2be234826f899ebf6f00f0255b27dc833
couto <tesye30@hotmail.com>
2022-02-10 23:55:02 -0300

    main imc a-26

--- modulo_2/aula26 - modelo copy/assets/js/main.js
+++ modulo_2/aula26 - modelo copy/assets/js/main.js
@@ -1,1 +1,7 @@
-// teste
+// Capturar evento de submit do formulário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('Evento previnido.');
+});
