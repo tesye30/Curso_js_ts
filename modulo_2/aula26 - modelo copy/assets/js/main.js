38840365b1b0e2dc728d75e0b1dfecc5bbc4bee2
couto <tesye30@hotmail.com>
2022-02-11 23:53:10 -0300

    main imc 1.1 a-26

--- modulo_2/aula26 - modelo copy/assets/js/main.js
+++ modulo_2/aula26 - modelo copy/assets/js/main.js
@@ -5,3 +5,12 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     console.log('Evento previnido.');
 });
+
+function setResultado (msg) {
+    const resultado = document.querySelector(#resultado);
+    resultado.innerHTML = '';
+    const p = document.createElement('p');
+    p.classList.add('paragrafo-resultado');
+    p.innerHTML = 'Qualquer coisa';
+    resultado.appendChild(p);
+}
