9c4605a8872509fb5184ea1b1824ddfeb7aa5793
couto <tesye30@hotmail.com>
2022-02-13 23:50:16 -0300

    imc function criarP e peso altura

--- modulo_2/aula26 - modelo copy/assets/js/main.js
+++ modulo_2/aula26 - modelo copy/assets/js/main.js
@@ -3,8 +3,22 @@ const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento previnido.');
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
+
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    
+    if (!peso) {
+        setResultado('Peso inválido')
+        return;
+    }
 });
+
+function criaP (){
+    const p = document.createElement('p');
+    return p;
+}
 
 function setResultado (msg) {
     const resultado = document.querySelector(#resultado);
