972fca4ef2747601c5a7d9602ba97ab37e504168
couto <tesye30@hotmail.com>
2022-04-02 23:57:42 -0300

    imc function criarP e peso altura 1.3

--- modulo_2/aula26 - modelo copy/assets/js/main.js
+++ modulo_2/aula26 - modelo copy/assets/js/main.js
@@ -19,13 +19,47 @@ form.addEventListener('submit', function (e) {
         return;
     }
     
-    console.log('')
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc)
+    console.log(nivelImc);
+        
 });
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) {
+        return nivel[5];
+    } 
+     if (imc >= 34.9) {
+        return nivel[4];
+    }
+    
+    if (imc >= 29.9) {
+        return nivel[3];
+    }
+     if (imc >= 24.9) {
+        return nivel[2];
+    }
+     if (imc >= 18.5) {
+        return nivel[1];
+    } 
+    if (imc < 18.5) {
+        return nivel[0]
+    }
+}
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
 
 function criaP (){
     const p = document.createElement('p');
     return p;
 }
+
+
 
 function setResultado (msg, isValid) {
     const resultado = document.querySelector('#resultado');
