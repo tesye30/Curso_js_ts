597e9c77909572e41e2f93e848b7fa3950ff8d98
couto <tesye30@hotmail.com>
2022-04-05 23:45:34 -0300

    operacao ternaria

---
+++ modulo_2/aula27/index.js
@@ -0,0 +1,11 @@
+// (condicao) ?  'Valor para verdadeiro' : 'Valor para falso'
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+console.log(nivelUsuario);
+
+/* if (pontuacaoUsuario >= 1000) {
+    console.log('Usuario VIP');
+} else {
+    console.log('Usuario normal');
+} */
+
