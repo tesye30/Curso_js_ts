25c9d1b82049127d0be5b9a57e871907e6313800
couto <tesye30@hotmail.com>
2022-05-11 23:49:44 -0300

    let e var diferenças - aula 31

---
+++ modulo_2/aula31/index.js
@@ -0,0 +1,14 @@
+
+const verdadeira = true;
+
+//Let te, escopo de bloco {... bloco }
+// Var só tem escopo de função
+
+let nome = 'Luiz';
+var nome2 = "Luiz";
+
+
+if (verdadeira){
+
+    console.log(nome, nome2)
+}
