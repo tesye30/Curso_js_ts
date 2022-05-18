60c367745167f6e4bbfe905144fb824787ad8363
couto <tesye30@hotmail.com>
2022-05-17 23:56:16 -0300

    atribuicao via desestruturacao (arrays)

---
+++ modulo_2/aula32/index.js
@@ -0,0 +1,5 @@
+//             0  1  2  3  4  5  6  7  8          
+const numeros = [10000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+
+const [um, dois, tres ] = numeros;
+console.log(um, dois, tres );
